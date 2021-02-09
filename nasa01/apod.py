ff11b1625d199c51c66bdac51249dbe5a201ad9b
li-lib <student@bchd.8d4adc86-be78-4e90-9b92-7988b9fee8a5>
2021-02-09 16:55:50 +0000

    NASA APOD

--- nasa01/apod.py
+++ nasa01/apod.py
@@ -5,7 +5,7 @@ from pprint import pprint as pp # part of the standard libray
 
 ## define some constants
 NASAAPI = 'https://api.nasa.gov/planetary/apod?' # this is our API to call
-MYKEY = 'api_key=DEMO_KEY' ## this is our api key
+MYKEY = 'api_key=FBMTDIoxDeBhHjcCdemRRdAYMMlRYC3baoN9Kvf9' ## this is our api key
 
 ## pretty print json
 def main():
